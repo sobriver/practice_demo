fac37a356347eb3463fb1f1ba044715fe7daf726
huangwei <huangwei@megvii.com>
2020-07-29 15:37:20 +0800

    11

---
+++ src/main/java/com/example/practice_demo/net/SendHttp.java
@@ -0,0 +1,44 @@
+package com.example.practice_demo.net;
+
+import okhttp3.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
+
+public class SendHttp {
+    public static void main(String[] args) {
+
+        OkHttpClient client = new OkHttpClient();
+
+        for (int i = 0; i < 150; i++) {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Request request = new Request
+                    .Builder() //利用建造者模式创建Request对象
+                    .url("https://www.acchw.top:9010/acc/app/mail/changePwd?email=sobriver@163.com")
+                    .addHeader("token", "eyJ0eXBlIjoiSldUIiwiYWxnIjoiSFM1MTIifQ.eyJzdWIiOiI5IiwiaWF0IjoxNTk2MDA3MjI5LCJleHAiOjE1OTg1OTkyMjl9.6F9xkfM4hZc9z-Fh9LwhYQUbEFHaUn8nsyN5Zbnen3aoRtCYw2SLum7C2_wc7qpjudMaKxDdCcX26jx2OfbSSw")
+                    .build();
+
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    System.out.println(e.getMessage());
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    System.out.println("success=" + response.body().string());
+                }
+            });
+
+        }
+
+    }
+
+
+}
--- src/main/java/com/example/practice_demo/test/Test.java
+++ src/main/java/com/example/practice_demo/test/Test.java
@@ -1,12 +1,35 @@
 package com.example.practice_demo.test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class Test {
     public static void main(String[] args) {
-        String s = "HHH%s%s%s%s";
-        System.out.println(String.format(s, 90, 989, "sdsd"));
+        Date date1 = new Date(122222220);
+        Date date2 = new Date(122222221);
+        Date date3 = new Date(122222224);
+
+        List<Date> list = new ArrayList<>();
+        list.add(date1);
+        list.add(date2);
+        list.add(date3);
+        Optional<Date> date = list.stream().max(Comparator.comparing(Date::getTime));
+        date.ifPresent(item -> System.out.println(item.getTime()));
+    }
+
+    private void t1() {
+        System.out.println("sdsd");
+        v2();
+    }
+
+    private void v2() {
+        System.out.println("sdsd");
+        v3();
+    }
+
+    private void v3() {
+        System.out.println("ssd");
+        throw new NullPointerException();
     }
 }
